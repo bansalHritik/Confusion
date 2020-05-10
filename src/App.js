8303477294057eeb6d1b43cf03af1278c80059f5
Ritik Bansal <sitesonlyrit@gmail.com>
2020-05-11 00:53:48 +0530

    React Component part 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Navbar,NavbarBrand} from 'reactstrap'
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from "./shared/dishes";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color = "primary">
-        <div className = "container">
-          <NavbarBrand href = "/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+  this.state = {
+    dishes : DISHES
+  };
+}
+
+
+  //This will create a empty navigation bar at the top
+  //This will create a bootStrap container with some predefined CSS
+  //Inside this container there is a navigation brand component 
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color = "primary"> 
+          <div className = "container">
+            <NavbarBrand href = "/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes  = {this.state.dishes}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
